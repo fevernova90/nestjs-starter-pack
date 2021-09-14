fd7f77e8b25629e9ccd41427ed0ee07d8bb2b1e2
Muhaimin <34376497+fevernova90@users.noreply.github.com>
2021-09-14 20:11:36 +0800

    Create default.entity.ts

---
+++ src/db/default.entity.ts
@@ -0,0 +1,21 @@
+import {
+  BaseEntity,
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class DefaultEntity extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn({ type: 'timestamp with time zone' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp with time zone' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ type: 'timestamp with time zone' })
+  deletedAt: Date;
+}
